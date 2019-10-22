5cb4899222dc03a3870e8eb764901f8aa96a9fdd
qiyubrother <qiyubrother@outlook.com>
2019-10-22 16:46:14 +0800

    添加项目文件。

---
+++ ScreenMonitor/FrmMain.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ScreenMonitor
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(106, 13);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(87, 33);
+            this.btnStop.TabIndex = 10;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(14, 13);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(87, 33);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.BackColor = System.Drawing.SystemColors.Control;
+            this.txtLog.Location = new System.Drawing.Point(14, 58);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(905, 577);
+            this.txtLog.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(199, 13);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(87, 33);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 651);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtLog);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Screen Monitor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ ScreenMonitor/FrmMain.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenMonitor
+{
+    public partial class FrmMain : Form
+    {
+        ScreenManager sm = new ScreenManager();
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            sm.Init();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            Text = "正在监控...";
+            txtLog.Text += $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff")}]正在监控...{Environment.NewLine}";
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Text = "监控已停止.";
+            txtLog.Text += $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff")}]监控已停止.{Environment.NewLine}";
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (sm.IsRuntimeScreenConfig())
+            {
+                var s = $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff")}] 屏幕配置发生了变化。{Environment.NewLine}";
+                s += $"原数据：{Environment.NewLine}";
+                s += sm.ToString();
+                var smNow = new ScreenManager();
+                smNow.Init();
+                s += $"新数据：{Environment.NewLine}";
+                s += smNow.ToString();
+                txtLog.Text += s;
+
+                sm = smNow;
+
+                txtLog.Text += Environment.NewLine;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtLog.Text = string.Empty;
+        }
+    }
+
+    public class ScreenManager
+    {
+        private List<ScreenInfo> _lst = new List<ScreenInfo>();
+        public List<ScreenInfo> GetRuntimeScreenConfig()
+        {
+            var lst = new List<ScreenInfo>();
+            for (var i = 0; i < Screen.AllScreens.Length; i++)
+            {
+                lst.Add(new ScreenInfo
+                {
+                    ScreenNo = i,
+                    DeviceName = Screen.AllScreens[i].DeviceName,
+                    IsPrimaryScreen = Screen.AllScreens[i].Primary
+                });
+            }
+
+            return lst;
+        }
+
+        public void Init()
+        {
+            _lst = GetRuntimeScreenConfig();
+        }
+
+        /// <summary>
+        /// 检测运行时，屏幕配置是否有变化
+        /// </summary>
+        /// <returns></returns>
+        public bool IsRuntimeScreenConfig()
+        {
+            var lstRuntime = GetRuntimeScreenConfig();
+            if (_lst.Count != lstRuntime.Count)
+            {
+                return true;
+            }
+            foreach (var s in _lst)
+            {
+                if (!lstRuntime.Any(x => x.ScreenNo == s.ScreenNo && x.DeviceName == s.DeviceName))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 返回控件所在的屏幕索引
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public int GetScreenNoByControl(Control c)
+        {
+            // 根据当前组件的位置查找组件所在屏幕的索引
+            var sc = _lst.FirstOrDefault(x => x.DeviceName == Screen.FromControl(c).DeviceName);
+            if (sc != null)
+            {
+                return sc.ScreenNo;
+            }
+            else
+            {
+                // 未找到，需要重新扫描显示器配置
+                return 0;
+            }
+        }
+
+
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            _lst.Sort(new ScreenInfoCompare());
+            for (var i = 0; i < _lst.Count; i++)
+            {
+                sb.AppendLine(_lst[i].ToString());
+            }
+            return sb.ToString();
+        }
+
+
+        private class ScreenInfoCompare : IComparer<ScreenInfo>
+        {
+            public int Compare(ScreenInfo x, ScreenInfo y)
+            {
+                if (x.ScreenNo < y.ScreenNo)
+                    return -1;
+                else if (x.ScreenNo == y.ScreenNo)
+                    return 0;
+                else
+                    return 1;
+            }
+        }
+    }
+    public class ScreenInfo
+    {
+        /// <summary>
+        /// 显示器的设备名
+        /// </summary>
+        public string DeviceName { get; set; }
+        /// <summary>
+        /// 显示器编号
+        /// </summary>
+        public int ScreenNo { get; set; }
+        /// <summary>
+        /// 是否是默认显示器
+        /// </summary>
+        public bool IsPrimaryScreen { get; set; }
+
+        public override string ToString()
+        {
+            return $"ScreenNo={ScreenNo}, DeviceName={DeviceName}, IsPrimaryScreen={IsPrimaryScreen}";
+        }
+    }
+}
